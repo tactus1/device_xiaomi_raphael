a08ecc4f2d8f17903b937d5b61566e162ffb075b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    raphael: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -603,7 +603,9 @@ AB_OTA_UPDATER := false
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
