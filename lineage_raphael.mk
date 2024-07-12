f06f3cb79b73796487c0c22256a8ac7469115eca
Tactus <142695427+tactus1@users.noreply.github.com>
2024-07-12 17:03:59 +0300

    lineage bringup

--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -15,16 +15,6 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Matrixx
-MATRIXX_BUILD_TYPE := Personal
-MATRIXX_MAINTAINER := NATO66613
-MATRIXX_CHIPSET := SM8150
-MATRIXX_BATTERY := 4000mAh
-MATRIXX_DISPLAY := 1080x2400
-WITH_GMS := true
-TARGET_HAS_UDFPS := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_ENABLE_BLUR := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_raphael
