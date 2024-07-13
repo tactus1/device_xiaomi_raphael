c8fdec44c2796856f1df88f90e1b3d56bf63cfb5
Tactus <142695427+tactus1@users.noreply.github.com>
2024-07-13 14:43:13 +0300

    raphael: Update COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_raphael-ap2a-user \
+    lineage_raphael-ap2a-userdebug \
+    lineage_raphael-ap2a-eng
