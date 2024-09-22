00f00b22bcdd76d9b2e93f6a36b7ef9cab7f28a2
tactus1 <tactus1@proton.me>
2024-09-22 21:49:17 +0000

    Remove Livedisplay

--- device.mk
+++ device.mk
@@ -328,10 +328,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# Livedisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.raphael
-
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
