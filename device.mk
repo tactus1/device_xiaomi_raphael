ab2827d8ccf8df3e6b6f342f88259b168b8908aa
Cyber Knight <cyberknight755@gmail.com>
2025-02-01 18:54:02 +0200

    raphael: Build DisplayFeatures
    - Add DisplayFeatures to PRODUCT_PACKAGES.
    - Address DisplayFeatures related SEPolicy rules.
    - Define sysfs_displayfeatures as a domain.
    - Allow system_app to read/write vendor_sysfs_graphics
    - Allow system_app to search vendor_sysfs_graphics
    - Setup permission of nodes relevant to DisplayFeatures at boot.
    - Setup DC Dim and HBM nodes as RRO for DisplayFeatures.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -240,6 +240,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# DisplayFeatures
+PRODUCT_PACKAGES += \
+    DisplayFeatures \
+    DisplayFeaturesRaphael
+
 # F2FS
 PRODUCT_PACKAGES += \
     sg_write_buffer \
