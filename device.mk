ef915544d142b52529376d267004b7cd818b11b7
mikeNG <mikeioannina@gmail.com>
2025-02-07 15:39:05 +0000

    raphael: Update display flags & packages
    Required after display HAL blueprint conversion

--- device.mk
+++ device.mk
@@ -203,8 +203,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.memtrack-service \
-    gralloc.msmnile \
-    hwcomposer.msmnile \
+    gralloc.qcom \
+    hwcomposer.qcom \
     libtinyxml \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
