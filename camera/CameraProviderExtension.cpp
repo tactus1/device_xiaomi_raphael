a2d37aed71f5dec169203c4fb63fdb04d4065f12
tactus1 <tactus1@proton.me>
2025-03-11 14:46:21 +0000

    Revert "raphael: camera: add enabled bool for finished workarounds"
    This reverts commit 52992f9674eb75326168f1e463a275c00fd746f2.

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -47,7 +47,7 @@ bool supportsSetTorchModeExt() {
 }
 
 int32_t getTorchDefaultStrengthLevelExt() {
-    return 80;
+    return 7;
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
@@ -62,17 +62,17 @@ int32_t getTorchStrengthLevelExt() {
     return get(node, 0);
 }
 
-void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
+void setTorchStrengthLevelExt(int32_t torchStrength) {
     set(TOGGLE_SWITCH, 0);
     for (auto& path : kTorchLedPaths) {
         auto node = path + "/" + TORCH_BRIGHTNESS;
         set(node, torchStrength);
     }
-    if (enabled)
+    if (torchStrength > 0)
         set(TOGGLE_SWITCH, 255);
 }
 
 void setTorchModeExt(bool enabled) {
     int32_t strength = getTorchDefaultStrengthLevelExt();
-    setTorchStrengthLevelExt(enabled ? strength : 0, enabled);
+    setTorchStrengthLevelExt(enabled ? strength : 0);
 }
