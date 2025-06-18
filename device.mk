3817d8f48bf29756d6a8a58a119b5cf7bb0ce0b6
kondors1995 <normandija1945@gmail.com>
2025-06-18 07:49:31 +0000

    raphael: update song namespace

--- device.mk
+++ device.mk
@@ -50,7 +50,9 @@ PRODUCT_SOONG_NAMESPACES += \
     bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/lineage/interfaces/light \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Variant Properties
 PRODUCT_COPY_FILES += \
